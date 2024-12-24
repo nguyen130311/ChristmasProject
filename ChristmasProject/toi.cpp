82ffd7b995be2ddcbd1b8cbecffe615f3a6c0cff
hvnguyen <winnguyen1303@gmail.com>
2024-12-24 20:35:11 +0700

    Add files via upload

---
+++ ChristmasProject/toi.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+void happyXmas()
+{
+    cout << '\n';
+    cout << '\n';
+    cout << '\n';
+    cout << "                      ********************************************************************************************\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                  *                                       *\n";
+    cout << "                      *                  *                                                                       *\n";
+    cout << "                      *                                                                          *               *\n";
+    cout << "                      *    *                                                                                     *\n";
+    cout << "                      *                                                       *                                  *\n";
+    cout << "                      *                                 *                                                        *\n";
+    cout << "                      *                                                 *           *                            *\n";
+    cout << "                      *                                                * *                        *              *\n";
+    cout << "                      *                            *                  *  *                                       *\n";
+    cout << "                      *                 *                            *   *                                       *\n";
+    cout << "                      *                                             *    *            *                          *\n";
+    cout << "                      *            *                               *    *                                  *     *\n";
+    cout << "                      *                                           *   **                                         *\n";
+    cout << "                      *                   *                       *****                                          *\n";
+    cout << "                      *                 *                                                                        *\n";
+    cout << "                      *                  *               *              *                      *                 *\n";
+    cout << "                      *                 * *                            * *                       *               *\n";
+    cout << "                      *         *****   ***   *         *****  *   *  *****  *   *     *   *   ***   *   *       *\n";
+    cout << "                      *           *    *   *  *           *    *   *  *      ** **     ** **  *   *  *   *       *\n";
+    cout << "                      *           *    *   *  *           *    *****  ***    * * *     * * *  *****  *   *       *\n";
+    cout << "                      *           *    *   *  *           *    *   *  *      *   *     *   *  *   *  *   *       *\n";
+    cout << "                      *           *     ***   *           *    *   *  *****  *   *     *   *  *   *   ***        *\n";
+    cout << "                      *                                            *                                             *\n";
+    cout << "                      *                                          *                                               *\n";
+    cout << "                      *                                        *   *                                             *\n";
+    cout << "                      *                                         ***                                              *\n";
+    cout << "                      *                          *****  ****    ***   *   *   ****                               *\n";
+    cout << "                      *                            *    *   *  *   *  **  *  *                                   *\n";
+    cout << "                      *                            *    ****   *****  * * *  * ***                               *\n";
+    cout << "                      *                            *    *  *   *   *  *  **  *   *                               *\n";
+    cout << "                      *                            *    *   *  *   *  *   *   ***                                *\n";
+    cout << "                      ********************************************************************************************\n";
+}
+int main() {
+
+    system("cls");
+    while(true) {    
+        happyXmas();
+        system("color 0A");
+        Sleep(567);
+        system("color 0B");
+        Sleep(567);
+        system("color 0C");
+        Sleep(567);        
+        system("color 0D");
+        Sleep(567);
+        system("color 0E");
+        Sleep(567);
+        system("color 0F");
+        Sleep(567);
+        system("color 04");
+        Sleep(567);
+        system("color 03");
+        Sleep(567);
+        system("cls");
+    }
+    return 0;
+}
---
+++ ChristmasProject/vuay.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+void happyXmas()
+{
+    cout << '\n';
+    cout << '\n';
+    cout << '\n';
+    cout << "                      ********************************************************************************************\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                  ****   ***    ***                      ***   *   *   ***                *\n";
+    cout << "                      *                 *      *   *  *   *                    *   *  *   *  *   *               *\n";
+    cout << "                      *                  ***   *****  *   *                    *      *****  *   *               *\n";
+    cout << "                      *                     *  *   *  *   *                    *   *  *   *  *   *               *\n";
+    cout << "                      *                 ****   *   *   ***                      ***   *   *   ***                *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                         11          11          11                                       *\n";
+    cout << "                      *                         11          11          11                                       *\n";
+    cout << "                      *                        11          11          11                                        *\n";
+    cout << "                      *                       111111      111111      111111                                     *\n";
+    cout << "                      *                       1111111     1111111     1111111                                    *\n";
+    cout << "                      *                        11111       11111       11111                                     *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                 *                        *                               *\n";
+    cout << "                      *                                   * *                  *                                 *\n";
+    cout << "                      *                        *    *  *   **   ***          *   *                               *\n";
+    cout << "                      *                        *    *  *   *   *   *          * *                                *\n";
+    cout << "                      *                        *    *  *   *   *****           *                                 *\n";
+    cout << "                      *                         *  *   *   *   *   *           *                                 *\n";
+    cout << "                      *                          **     ***    *   *           *                                 *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      *                                                                                          *\n";
+    cout << "                      ********************************************************************************************\n";
+}
+int main() {
+
+    system("cls");
+    while(true) {    
+        happyXmas();
+        system("color 0A");
+        Sleep(567);
+        system("color 0B");
+        Sleep(567);
+        system("color 0C");
+        Sleep(567);        
+        system("color 0D");
+        Sleep(567);
+        system("color 0E");
+        Sleep(567);
+        system("color 0F");
+        Sleep(567);
+        system("color 04");
+        Sleep(567);
+        system("color 03");
+        Sleep(567);
+        system("cls");
+    }
+    return 0;
+}
